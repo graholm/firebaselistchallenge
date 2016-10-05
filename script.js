7e5120384594fa3454908b3c90ac969a9a5f6f8f
graholm <graholm@nuevaschool.org>
2016-10-05 08:44:09 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var input = $("#new-item-input")
 input.keydown(function(e) {	
   if (e.keyCode == "13") {
-    console.log(e.keyCode)
     var valueToSave = input.val();
     addListItem(valueToSave);
   }
 })
+
 
 onNewListItem("graceList", function(value) {
   $("#list-display").append("<div>" + value + "</div>")
